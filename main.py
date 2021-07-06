ec19b21d83fa85db35d12fd4d40d4dcb6853be62
NikhilB19 <nban982@aucklanduni.ac.nz>
2021-07-06 16:28:32 +1200

    Can display graphs now (may have calculation errors)

--- main.py
+++ main.py
@@ -5,30 +5,23 @@ from glob import glob
 from datetime import date
 from pandas._libs.tslibs import NaT
 import yfinance as yf
+from matplotlib import pyplot as plt
+
+# Potential problem: does not take into account non-trade fees (Stake Black).
 
 
-
-
-# Reads in trades from hatch as pandas dataframe
-hatchFilePath = glob('trade reports' + os.sep + 'hatch' + os.sep + '*.csv')
-hatchTrades = pd.read_csv(hatchFilePath[0])
-
-
-# Reads in trades from Stake as pandasdataframe 
-stakeFilePath = glob('trade reports' + os.sep + 'stake' + os.sep + '*.xlsx')
-stakeTrades = pd.read_excel(stakeFilePath[0], sheet_name='Trades')
-
-
-# Reads in trades from Sharesies 
-# UNDER CONSTRUCTION
-
-
+# Reads in trades from hatch as pandas dataframe. File format works as of 5/7/21.
+hatchTradeFilePath = glob('trade reports' + os.sep + 'hatch' + os.sep + 'order-transaction*.csv')
+hatchTrades = pd.read_csv(hatchTradeFilePath[0])
 # Simplifying hatchTrades dataframe.
 hatchTrades = hatchTrades.assign(Fees=3) # Forms a new column containing the flat $3 fee for each trade.
 hatchTrades = hatchTrades.drop(['Comments'], axis=1) # Removes the comments column.
 hatchTrades = hatchTrades.rename({'Instrument Code' : 'Ticker', 'Transaction Type' : 'Type'}, axis=1)
-# hatchTrades['Trade Date'] = pd.to_datetime(hatchTrades['Trade Date'], format = '%d%M%Y')
 
+
+# Reads in trades from Stake as pandasdataframe. File format works as of 5/7/21.
+stakeFilePath = glob('trade reports' + os.sep + 'stake' + os.sep + '*.xlsx')
+stakeTrades = pd.read_excel(stakeFilePath[0], sheet_name='Trades')
 # Simplifying stakeTrades dataframe.
 # Removing useless data:
 stakeTrades = stakeTrades.drop(['DATE (US)', 'REFERENCE', 'TAF FEE (USD)', 'SEC FEE (USD)', 'VALUE (USD)',
@@ -39,20 +32,82 @@ stakeTrades = stakeTrades.rename({'SETTLEMENT DATE (US)' : 'Trade Date', 'SIDE' 
                                 'BROKERAGE FEE (USD)' : 'Fees', 'SYMBOL' : 'Ticker'}, axis=1) 
 # Reassigning B/S values in Type to BUY/SELL to match hatch convention:
 stakeTrades['Type'] = np.where(stakeTrades['Type'] == 'B', 'BUY', 'SELL')
-# stakeTrades['Trade Date'] = pd.to_datetime(stakeTrades['Trade Date'], format = '%Y%M%d')
 
 
-# Combining dataframes
+# Reads in trades from Sharesies 
+# UNDER CONSTRUCTION
+
+
+# Combining trades dataframes
 trades = pd.concat([hatchTrades, stakeTrades], ignore_index=True)
 # Making Trade date be stored as datetime.
 trades['Trade Date'] = pd.to_datetime(trades['Trade Date'])
 
+
+# Reads in deposit data from Hatch
+# The .csv file to be read is manually made. Hatch does not provide any deposit information as of 5/7/21.
+hatchDepositFilePath = glob('trade reports' + os.sep + 'hatch' + os.sep + 'Hatch Deposit Data.csv')
+hatchDeposits = pd.read_csv(hatchDepositFilePath[0])
+
+
+# Reads in deposit data from Stake
+# The provided Stake report does not contain the NZD amounts as of 5/7/21. This needs to be added manually.
+stakeDepositFilePath = stakeFilePath
+stakeDeposits = pd.read_excel(stakeDepositFilePath[0], sheet_name='Deposits & Withdrawals')
+# Simplifying stakeDeposits dataframe:
+# Removing useless data.
+stakeDeposits = stakeDeposits.drop(['REFERENCE', 'FUNDING METHOD'], axis=1)
+# Renaming data.
+stakeDeposits = stakeDeposits.rename({'DATE (US)' : 'Date', 'FUNDING TYPE' : 'Type', 
+                                    'RECEIVE AMOUNT (USD)' : 'USD Quantity'}, axis=1)
+
+
+# Reads in deposit data from Sharesies
+# UNDER CONSTRUCTION
+
+
+# Combining deposits dataframes
+deposits = pd.concat([hatchDeposits, stakeDeposits], ignore_index=True)
+# Making date be stored as datetime.
+deposits['Date'] = pd.to_datetime(deposits['Date'])
+
 # Finding the min date.
-fromDate = trades['Trade Date'].min()
+fromDate = deposits['Date'].min()
 
 # Finding today's date.
 todayDate = date.today()
 
+
+# Imports USD exchange data.
+adjCloseUSD = yf.download('NZDUSD=X', start=fromDate, endDate=todayDate)['Adj Close']
+
+
+# Forms pandas dataframe containing amount of USD held over time.
+# 'Total' should potentially be a total NZD value invested. heldUSD may need renaming!!!!!!!!!!!
+cash = pd.DataFrame(index=adjCloseUSD.index, data=0, columns = ['USD cash held', 'NZD invested', 'initial USD bought'])
+for i in deposits.index:
+    if deposits['Type'][i] == 'Deposit':
+        buyDate = deposits['Date'][i]
+        boughtUSD = deposits['USD Quantity'][i]
+        soldNZD = deposits['NZD Quantity'][i]
+
+        cash.loc[buyDate:,'USD cash held'] += boughtUSD 
+        cash.loc[buyDate:,'NZD invested'] += soldNZD
+
+    
+    elif deposits['Type'][i] == 'Withdrawal':
+        sellDate = deposits['Date'][i]
+        soldUSD = deposits['USD Quantity'][i]
+        boughtNZD = deposits['NZD Quantity'][i]
+
+        cash.loc[sellDate:, 'USD cash held'] -= soldUSD
+        cash.loc[sellDate:, 'NZD invested'] -= boughtNZD
+    
+    else:
+        raise ValueError('Invalid Deposit type')
+
+# Initializing the initial USD bought column.
+cash['initial USD bought'] = cash['USD cash held']
 
 # Building string parameter to pass to yf.download containing all tickers.
 # Unique stocks:
@@ -66,25 +121,107 @@ for stock in range(1, len(uniqueStocks)):
 adjCloseData = yf.download(tickers, start=fromDate, end=todayDate)['Adj Close']
 
 
-# Forming pandas series containing units of stock for each stock.
+# Forming pandas dataframe containing units of stock for each stock.
 unitsData = adjCloseData * 0
 for stock in uniqueStocks:
     # Adds stock quantity from buy date to end for each buy order. Treats multiple buy orders in 1 day as 1 order.
     buyOrders = trades[(trades['Type'] == 'BUY') & (trades['Ticker'] == stock)]['Trade Date'].unique()
-
     for buyOrder in buyOrders:
         currentUnits = np.sum(trades[(trades['Type'] == 'BUY') & (trades['Ticker'] == stock) 
                         & (trades['Trade Date'] == buyOrder)]['Quantity'].to_numpy())
-        unitsData.loc[buyOrder:][stock] += currentUnits
+        unitsData.loc[buyOrder:, stock] += currentUnits
     
     # Substracts stock quantity from sell date to end for each sell order. Treats multiple sell orders in 1 day as 1 order.
     sellOrders = trades[(trades['Type'] == 'SELL') & (trades['Ticker'] == stock)]['Trade Date'].unique()
     for sellOrder in sellOrders:
         currentUnits = np.sum(trades[(trades['Type'] == 'SELL') & (trades['Ticker'] == stock) 
                         & (trades['Trade Date'] == sellOrder)]['Quantity'].to_numpy())
-        unitsData.loc[sellOrder:][stock] -= currentUnits
+        unitsData.loc[sellOrder:, stock] -= currentUnits
 
 investedStockVal = unitsData * adjCloseData
-investedStockVal['Total'] = investedStockVal.sum(axis=1)
+investedStockVal['USD stock value'] = investedStockVal.sum(axis=1)
+
+
+# Forming the USD cash held column of cash.
+for i in trades.index:
+    if (trades['Type'][i] == 'BUY'):
+        stockUnits = trades['Quantity'][i]
+        priceBuy = trades['Price'][i]
+        usdUsed = stockUnits * priceBuy
+        
+        fees = trades['Fees'][i]
+
+        dateBought = trades['Trade Date'][i]
+
+        cash.loc[dateBought:, 'USD cash held'] -= usdUsed + fees
+    
+    elif (trades['Type'][i] == 'SELL'):
+        stockUnits = trades['Quantity'][i]
+        priceSell = trades['Price'][i]
+        usdUsed = stockUnits * priceSell
+
+        fees = trades['Fees'][i]
+
+        dateSold = trades['Trade Date'][i]
+
+        cash.loc[dateSold:, 'USD cash held'] += usdUsed - fees
+    
+    else:
+        raise ValueError('Invalid Order type')
+
+
+# Creating infoNZD dataframe containing summarised important information in NZD.
+infoNZD = pd.DataFrame(index=investedStockVal.index)
+infoNZD['Total value of USD and stocks ($NZD)'] = (investedStockVal['USD stock value'] + cash['USD cash held']) / adjCloseUSD
+infoNZD['Total initial NZD invested ($NZD)'] = cash['NZD invested']
+infoNZD['Profit/Loss ($NZD)'] = infoNZD['Total value of USD and stocks ($NZD)'] - infoNZD['Total initial NZD invested ($NZD)']
+infoNZD['% Profit/Loss'] = infoNZD['Profit/Loss ($NZD)'] / infoNZD['Total initial NZD invested ($NZD)']
+
+# Creating infoUSD dataframe containing summarised important information in USD.
+infoUSD = pd.DataFrame(index=investedStockVal.index)
+infoUSD['Total value of USD and stocks ($USD)'] = investedStockVal['USD stock value'] + cash['USD cash held']
+infoUSD['Total initial USD invested ($USD)'] = cash['initial USD bought']
+infoUSD['Profit/Loss ($USD)'] = infoUSD['Total value of USD and stocks ($USD)'] - infoUSD['Total initial USD invested ($USD)']
+infoUSD['% Profit/Loss'] = infoUSD['Profit/Loss ($USD)'] / infoUSD['Total initial USD invested ($USD)']
+
+
+# Forming the NZD plots
+fig1, axs1 = plt.subplots(3)
+fig1.suptitle('Portolio Tracker ($NZD)')
+
+axs1[0].plot(infoNZD.index, infoNZD['Total value of USD and stocks ($NZD)'], 'b-', label = 'Portfolio Value ($NZD)')
+axs1[0].plot(infoNZD.index, infoNZD['Total initial NZD invested ($NZD)'], 'y-', label = 'My Contribution ($NZD)')
+
+axs1[1].plot(infoNZD.index, infoNZD['Profit/Loss ($NZD)'], 'g-', label = 'Profit/Loss ($NZD)')
+
+axs1[2].plot(infoNZD.index, infoNZD['% Profit/Loss'], 'r-', label = '% Profit/Loss')
+
+axs1[0].legend()
+axs1[1].legend()
+axs1[2].legend()
+
+
+# Forming the USD plots
+fig2, axs2 = plt.subplots(3)
+fig2.suptitle('Portolio Tracker ($USD)')
+
+axs2[0].plot(infoUSD.index, infoUSD['Total value of USD and stocks ($USD)'], 'b-', label = 'Portfolio Value ($USD)')
+axs2[0].plot(infoUSD.index, infoUSD['Total initial USD invested ($USD)'], 'y-', label = 'My Contribution ($USD)')
+
+axs2[1].plot(infoUSD.index, infoUSD['Profit/Loss ($USD)'], 'g-', label = 'Profit/Loss ($USD)')
+
+axs2[2].plot(infoUSD.index, infoUSD['% Profit/Loss'], 'r-', label = '% Profit/Loss')
+
+axs2[0].legend()
+axs2[1].legend()
+axs2[2].legend()
+
+
+plt.show()
+
+
+
+
+
 
 pass
